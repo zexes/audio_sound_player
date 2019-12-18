89f6292bcf25454b6a8b19ac6a93e752690a8132
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2019-12-18 19:03:05 +0100

    audio alone

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:audioplayers/audio_cache.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
@@ -33,106 +35,103 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        backgroundColor: Colors.white70,
-        appBar: AppBar(
-          backgroundColor: Colors.brown,
-          title: Text('zikozee Audio Player'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.cloud),
-              ),
-              Tab(
-                icon: Icon(Icons.phone_android),
-              ),
-              Tab(icon: Icon(Icons.web_asset)),
-            ],
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          backgroundColor: Colors.white70,
+          appBar: AppBar(
+            backgroundColor: Colors.brown,
+            title: Text('zikozee Audio Player'),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.phone_android),
+                ),
+                Tab(icon: Icon(Icons.web_asset)),
+              ],
+            ),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            Tab(child: Icon(Icons.cloud)),
-            Tab(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      child: Text('$filePath'),
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    ButtonTheme(
-                      minWidth: 200.0,
-                      height: 60.0,
-                      child: RaisedButton(
-                        onPressed: () async {
-                          path = await getFileName();
-                          setState(() {
-                            filePath = path;
-                            print('value is now $filePath');
-                          });
-                        },
-                        child: Text('Select Audio'),
-                        color: Colors.lightGreen,
+          body: TabBarView(
+            children: <Widget>[
+              Tab(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        child: Text('$filePath'),
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        FlatButton(
-                          onPressed: () async => await audioPlayer.stop(),
-                          child: Icon(
-                            Icons.stop,
-                            color: Colors.blueGrey[900],
-                            size: 100,
-                          ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      ButtonTheme(
+                        minWidth: 200.0,
+                        height: 60.0,
+                        child: RaisedButton(
+                          onPressed: () async {
+                            path = await getFileName();
+                            setState(() {
+                              filePath = path;
+                              print('value is now $filePath');
+                            });
+                          },
+                          child: Text('Select Audio'),
+                          color: Colors.lightGreen,
                         ),
-                        FlatButton(
-                          onPressed: () async =>
-                              await audioPlayer.play(filePath, isLocal: true),
-                          child: Icon(
-                            Icons.play_arrow,
-                            color: Colors.blueGrey[900],
-                            size: 100,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          FlatButton(
+                            onPressed: () async => await audioPlayer.stop(),
+                            child: Icon(
+                              Icons.stop,
+                              color: Colors.blueGrey[900],
+                              size: 100,
+                            ),
                           ),
-                        ),
-                        FlatButton(
-                          onPressed: () async => await audioPlayer.pause(),
-                          child: Icon(
-                            Icons.pause,
-                            color: Colors.blueGrey[900],
-                            size: 100,
+                          FlatButton(
+                            onPressed: () async =>
+                                await audioPlayer.play(filePath, isLocal: true),
+                            child: Icon(
+                              Icons.play_arrow,
+                              color: Colors.blueGrey[900],
+                              size: 100,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
+                          FlatButton(
+                            onPressed: () async => await audioPlayer.pause(),
+                            child: Icon(
+                              Icons.pause,
+                              color: Colors.blueGrey[900],
+                              size: 100,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Tab(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    soundPlayer(color: Colors.red, soundNumber: 1),
+                    soundPlayer(color: Colors.deepOrange, soundNumber: 2),
+                    soundPlayer(color: Colors.yellow, soundNumber: 3),
+                    soundPlayer(color: Colors.green, soundNumber: 4),
+                    soundPlayer(color: Colors.teal, soundNumber: 5),
+                    soundPlayer(color: Colors.blue, soundNumber: 6),
+                    soundPlayer(color: Colors.purple, soundNumber: 7),
                   ],
                 ),
               ),
-            ),
-            Tab(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  soundPlayer(color: Colors.red, soundNumber: 1),
-                  soundPlayer(color: Colors.deepOrange, soundNumber: 2),
-                  soundPlayer(color: Colors.yellow, soundNumber: 3),
-                  soundPlayer(color: Colors.green, soundNumber: 4),
-                  soundPlayer(color: Colors.teal, soundNumber: 5),
-                  soundPlayer(color: Colors.blue, soundNumber: 6),
-                  soundPlayer(color: Colors.purple, soundNumber: 7),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 
   Future<String> getFileName() async {
